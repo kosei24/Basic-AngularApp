1b94b1d771977fbc5c188da92db9e57cc342a2e1
kosei <higashi.k24@gmail.com>
2021-06-12 08:43:27 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,53 @@
+export const products = [
+    {
+        coverImage:"./assets/img/phone-cover.jpg",
+        name: "Phone XL",
+        price: 799,
+        description: "A large phone with one of the best screens",
+        heading1:"ヘッド1",
+        heading2:"ヘッド2",
+        heading3:"ヘッド3",
+        headingtext1:"あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ",
+        headingtext2:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+        headingtext3:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+    },
+
+    {
+        coverImage:"./assets/img/phone-cover.jpg",
+        name: "Phone Mini",
+        price: 699,
+        description: "A great phone with one of the best camera",
+        heading1:"ヘッド1",
+        heading2:"ヘッド2",
+        heading3:"ヘッド3",
+        headingtext1:"あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ",
+        headingtext2:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+        headingtext3:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+    },
+
+    {
+        coverImage:"./assets/img/phone-cover.jpg",
+        name: "Phone standard",
+        price: 299,
+        description: "Normal",
+        heading1:"ヘッド1",
+        heading2:"ヘッド2",
+        heading3:"ヘッド3",
+        headingtext1:"あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ",
+        headingtext2:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+        headingtext3:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+    },
+
+    {
+        coverImage:"./assets/img/phone-cover.jpg",
+        name: "Phone Special",
+        price: 999,
+        description: "Special",
+        heading1:"ヘッド1",
+        heading2:"ヘッド2",
+        heading3:"ヘッド3",
+        headingtext1:"あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ あいうえおかきくけこさしすせそ",
+        headingtext2:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+        headingtext3:"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+    }
+];
