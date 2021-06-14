edd9a9c5511ebb132ec074f515d3a9d597552fe7
kosei <higashi.k24@gmail.com>
2021-06-15 08:55:54 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const express = require("express")
 //MongoDB接続用のモジュール(https://www.npmjs.com/package/mongoose)
 const mongoose = require('mongoose')
 // パスワードをgitに載せないようにする
-const config = require("./config/dev")
+const config = require("./config/index")
 // Fake data
 const FakeDb = require("./fake-db")
 // エンドポイントのルーター
 const productRoutes = require("./routes/products")
+// ディレクトリ
+const path = require("path")
 
 
 //mongooseを用いてMongoDBに接続
@@ -18,10 +20,12 @@ mongoose.connect(config.DB_URI, {
   useCreateIndex: true
 }).then(
   () => {
-    //DBに繋いだ後の処理を定義 - インスタンス生成
-    const fakeDb = new FakeDb()
-    // DB保存
-    fakeDb.initDb()
+    if (process.env.NODE_ENV !== "production"){
+      //DBに繋いだ後の処理を定義 - インスタンス生成
+      const fakeDb = new FakeDb()
+      // DB保存
+      // fakeDb.initDb()
+    }
 
   }
 )
@@ -33,6 +37,16 @@ const app = express()
 
 //エンドポイント呼び出し
 app.use("/api/v1/products", productRoutes)
+
+//(本番環境対応)上記エンドポイント意外のエンドポイントのリクエストが来たときは、index.htmlのレスポンスを返す
+if (process.env.NODE_ENV == "production"){
+const appPath = path.join(__dirname,"..","dist","Basic-AngularApp")
+app.use(express.static(appPath))
+app.get("*", function(req,res) {
+  res.sendFile(path,resolve(appPath,"index.html"))
+})
+}
+
 
 
 //クラウドにPORTを繋がる場合はprocessを作動し、ローカルならば3001番ポートに繋ぐ
