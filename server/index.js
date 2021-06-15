5191dd44bb380d7e84f8fcabae78a890cee01c4e
kosei <higashi.k24@gmail.com>
2021-06-15 10:19:10 +0900

    index.js修正

--- server/index.js
+++ server/index.js
@@ -12,50 +12,80 @@ const productRoutes = require("./routes/products")
 const path = require("path")
 
 
-//mongooseを用いてMongoDBに接続
-mongoose.connect(config.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-}).then(
-  () => {
-    if (process.env.NODE_ENV !== "production"){
-      //DBに繋いだ後の処理を定義 - インスタンス生成
-      const fakeDb = new FakeDb()
-      // DB保存
-      // fakeDb.initDb()
-    }
+// //mongooseを用いてMongoDBに接続
+// mongoose.connect(config.DB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// }).then(
+//   () => {
+//     if (process.env.NODE_ENV !== "production"){
+//       //DBに繋いだ後の処理を定義 - インスタンス生成
+//       const fakeDb = new FakeDb()
+//       // DB保存
+//       // fakeDb.initDb()
+//     }
 
+//   }
+// )
+
+
+// // express インスタンス
+// const app = express()
+
+
+// //エンドポイント呼び出し
+// app.use("/api/v1/products", productRoutes)
+
+// //(本番環境対応)上記エンドポイント意外のエンドポイントのリクエストが来たときは、index.htmlのレスポンスを返す
+// if (process.env.NODE_ENV == "production"){
+// const appPath = path.join(__dirname,"..","dist","Basic-AngularApp")
+// app.use(express.static(appPath))
+// app.get("*", function(req,res) {
+//   res.sendFile(path,resolve(appPath,"index.html"))
+// })
+// }
+
+
+
+// //クラウドにPORTを繋がる場合はprocessを作動し、ローカルならば3001番ポートに繋ぐ
+// const PORT = process.env.PORT || "3001"
+
+
+// //nodeサーバー起動
+// app.listen(PORT, function(){
+//     console.log("I am running")
+// })
+
+
+ongoose.connect(config.DB_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true
+}).then(
+() => {
+  if(process.env.NODE_ENV !== 'production') {
+    const sampleDb = new SampleDb()
+    // sampleDb.initDb()
   }
+}
 )
 
-
-// express インスタンス
 const app = express()
 
+app.use('/api/v1/products', productRoutes)
 
-//エンドポイント呼び出し
-app.use("/api/v1/products", productRoutes)
-
-//(本番環境対応)上記エンドポイント意外のエンドポイントのリクエストが来たときは、index.htmlのレスポンスを返す
-if (process.env.NODE_ENV == "production"){
-const appPath = path.join(__dirname,"..","dist","Basic-AngularApp")
+if(process.env.NODE_ENV === 'production') {
+const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
 app.use(express.static(appPath))
-app.get("*", function(req,res) {
-  res.sendFile(path,resolve(appPath,"index.html"))
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(appPath, 'index.html'))
 })
 }
 
 
+const PORT = process.env.PORT || '3001'
 
-//クラウドにPORTを繋がる場合はprocessを作動し、ローカルならば3001番ポートに繋ぐ
-const PORT = process.env.PORT || "3001"
-
-
-//nodeサーバー起動
-app.listen(PORT, function(){
-    console.log("I am running")
+app.listen(PORT, function() {
+console.log('I am running!')
 })
-
-
